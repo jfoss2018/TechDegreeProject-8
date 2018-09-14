964ad74c327442faf8d86c8ae59d2aadbb0b19b4
James Foss <jamesfoss1@hotmail.com>
2018-09-14 17:24:54 -0400

    Project Complete

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # TechDegreeProject-8
+
+Treehouse Project #8
+
+This project focuses on setting up Gulp as a task manager to streamline the workflow of the user.
+
+To use this project file, you must file install all dependencies by running "npm install."
+
+Once all dependencies are installed, the following tasks will be available to run:
+1. gulp scripts - Concatenates and minifies .js files. Includes a map file.
+2. gulp styles - Compiles .scss files into a .css file and minifies. Includes a map file.
+3. gulp images - Compresses images.
+4. gulp clean - Deletes all files/folders that were created by tasks.
+5. gulp build - Runs the clean task before running all other tasks to build a complete production folder (dist).
+6. gulp - Runs the build task before serving the files and watching for changes to .scss files.
--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,89 @@
-// As a developer, I should be able to run the npm install command to install
-// all of the dependencies for the build process.
-
-// As a developer, I should be able to run the gulp scripts command at the
-// command line to concatenate, minify, and copy all of the project’s JavaScript
-// files into an all.min.js file that is then copied to the dist/scripts folder.
-
-// As a developer, I should be able to run the gulp styles command at the
-// command line to compile the project’s SCSS files into CSS, then concatenate
-// and minify into an all.min.css file that is then copied to the dist/styles
-// folder.
-
-// As a developer, when I run the gulp scripts or gulp styles commands at the
-// command line, source maps are generated for the JavaScript and CSS files
-// respectively.
-
-// As a developer, I should be able to run the gulp images command at the
-// command line to optimize the size of the project’s JPEG and PNG files, and
-// then copy those optimized images to the dist/content folder.
-
-// As a developer, I should be able to run the gulp clean command at the command
-// line to delete all of the files and folders in the dist folder.
-
-// As a developer, I should be able to run the gulp build command at the command
-// line to run the clean, scripts, styles, and images tasks with confidence that
-// the clean task completes before the other commands.
-
-// As a developer, I should be able to run the gulp command at the command line
-// to run the build task and serve my project using a local web server.
-
-// Extra Credit: As a developer, when I run the default gulp command, it should
-// continuously watch for changes to any .scss file in my project. When there is
-// a change to one of the .scss files, the gulp styles command is run and the
-// files are compiled, concatenated, and minified to the dist folder. My project
-// should then reload in the browser, displaying the changes.
-
-
+// Required modules.
 const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const maps = require('gulp-sourcemaps');
+const csso = require('gulp-csso');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const server = require('gulp-server-livereload');
+const useref = require('gulp-useref');
+const replace = require('gulp-replace');
+const gulpSequence = require('gulp-sequence');
+const iff = require('gulp-if');
+
+// Concats all .js files including jQuery and creates maps.
+gulp.task('concatjs', function() {
+  return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'js/circle/autogrow.js', 'js/circle/circle.js'])
+    .pipe(maps.init())
+    .pipe(concat('global.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(gulp.dest('js'));
+});
+
+// Minifies .js files.
+gulp.task('scripts', ['concatjs'], function() {
+  return gulp.src('dist/scripts/global.js')
+    .pipe(uglify())
+    .pipe(rename('all.min.js'))
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+// Compiles scss into css and creates maps.
+gulp.task('compileSass', function() {
+  return gulp.src('sass/global.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(gulp.dest('css'));
+});
+
+// Minifies css files.
+gulp.task('styles', ['compileSass'], function() {
+  return gulp.src('dist/styles/global.css')
+    .pipe(csso())
+    .pipe(rename('all.min.css'))
+    .pipe(gulp.dest('dist/styles'));
+});
+
+// compresses images and stores them into the dist/contents folder
+gulp.task('images', function() {
+  return gulp.src('images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/content'));
+});
+
+// Moves the index.html file and icons folder into the dist folder.
+// Also replaces file path for images to the contents folder, and effectively
+// does the same for .js & .css using useref.
+gulp.task('move', ['scripts', 'styles'], function() {
+  return gulp.src(['index.html', 'icons/*'], {base: './'})
+    .pipe(iff('*.html', replace('images/','content/')))
+    .pipe(iff('*.html', useref()))
+    .pipe(gulp.dest('dist'));
+});
+
+// deletes the dist and css folders and the .js file inside the js folder.
+gulp.task('clean', function() {
+  return del(['dist', 'js/*.js*', 'css']);
+});
+
+// runs the clean task, then the scripts, styles, and images tasks in parallel,
+// then the move task.
+gulp.task('build', gulpSequence('clean', ['scripts', 'styles', 'images'], 'move'));
+
+// The default task runs the build task before setting up the server and watching
+// for changes to any .scss file.
+gulp.task('default', ['build'], function() {
+  gulp.src('dist')
+    .pipe(server({
+      port: 3000,
+      open: true,
+      livereload: true
+    }))
+  gulp.watch('sass/**/*.scss', ['styles']);
+});
